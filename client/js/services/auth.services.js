1b78576c88ff64cdbcf8f8abed469b7c284df51f
ocph23@gmail.com <ocph23@gmail.com>
2020-04-22 11:00:41 +0900

    aa

--- client/js/services/auth.services.js
+++ client/js/services/auth.services.js
@@ -85,7 +85,7 @@ function AuthService($http, $q, StorageService, $state, helperServices, message)
 			data: user
 		}).then(
 			(res) => {
-				StorageService.addObject('user', res.data.data);
+				StorageService.addObject('user', res.data);
 				def.resolve(res.data);
 			},
 			(err) => {
